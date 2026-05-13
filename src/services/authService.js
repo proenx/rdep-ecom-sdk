0a99d3c4cc968b251d337e513754f5da0ee4e5b0
Shivani Biswas <shivani.b@proenx.com>
2026-05-13 18:13:36 +0530

    added subdomain api

--- src/services/authService.js
+++ src/services/authService.js
@@ -16,24 +16,74 @@ export const getSubDomain = () => {
 };
 
 /**
+ * Get Tenant Subdomain
+ */
+export const getTenantSubDomain = () => {
+  const host = window.location.host;
+
+  const parts = host.split(".");
+
+  const domainLength = 3;
+
+  return parts.length >= domainLength && parts[0] !== "www"
+    ? parts[0]
+    : import.meta.env.NUXT_PUBLIC_SUB_DOMAIN;
+};
+
+/**
+ * Check Tenant API
+ */
+export const checkTenant = async () => {
+  const tenantSubDomain = getTenantSubDomain();
+
+  // save globally
+  setSubDomain(tenantSubDomain);
+
+  if (tenantSubDomain !== "superadmin") {
+    try {
+      const uri = `/auth-service/noauth/tenant/check/${tenantSubDomain}`;
+
+      const res = await apiClient.get(uri);
+
+      return res?.data || {};
+    } catch (error) {
+      console.error(
+        "Tenant Check Error:",
+        error?.response?.data || error.message,
+      );
+
+      throw error;
+    }
+  }
+
+  return {};
+};
+
+/**
  * Login
  */
 export const login = async ({ username, password, subDomain }) => {
   // save subdomain state
   setSubDomain(subDomain);
+
   const res = await apiClient.post("/auth-service/ui/auth", {
     username,
     password,
     tenantSubDomain: subDomain,
   });
+
   const token = res?.headers?.authorization || res?.headers?.Authorization;
+
   if (token) {
     setToken(token);
   }
+
   const user = res?.data || {};
+
   if (user) {
     setUserDetails(user);
   }
+
   return user;
 };
 
