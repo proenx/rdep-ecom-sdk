7fb30e8b70e20cfdd1b4cf22f2f711349840cc93
Shivani Biswas <shivani.b@proenx.com>
2026-05-13 13:57:58 +0530

    added subdomain api

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,26 +2,44 @@ import apiClient from "../core/apiClient.js";
 import { setToken, clearToken } from "../core/tokenManager.js";
 import { setUserDetails, clearUserDetails } from "../core/userDetails.js";
 
+/**
+ * Subdomain State
+ */
+let currentSubDomain = null;
+
+export const setSubDomain = (subDomain) => {
+  currentSubDomain = subDomain;
+};
+
+export const getSubDomain = () => {
+  return currentSubDomain;
+};
+
+/**
+ * Login
+ */
 export const login = async ({ username, password, subDomain }) => {
+  // save subdomain state
+  setSubDomain(subDomain);
   const res = await apiClient.post("/auth-service/ui/auth", {
     username,
     password,
     tenantSubDomain: subDomain,
   });
-
   const token = res?.headers?.authorization || res?.headers?.Authorization;
   if (token) {
     setToken(token);
   }
-
   const user = res?.data || {};
   if (user) {
     setUserDetails(user);
   }
-
   return user;
 };
 
+/**
+ * Logout
+ */
 export const logout = async () => {
   try {
     await apiClient.get("/auth-service/ui/logout");
@@ -30,5 +48,8 @@ export const logout = async () => {
   } finally {
     clearToken();
     clearUserDetails();
+
+    // clear subdomain
+    currentSubDomain = null;
   }
 };
